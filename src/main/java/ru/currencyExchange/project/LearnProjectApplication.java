58a93fffa61465aa05e9de59620bee7db879c897
Maksim <nersus1337@mail.ru>
2024-01-25 15:06:29 +0700

    Added sqlite

--- src/main/java/ru/currencyExchange/project/LearnProjectApplication.java
+++ src/main/java/ru/currencyExchange/project/LearnProjectApplication.java
@@ -1,7 +1,13 @@
 package ru.currencyExchange.project;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class LearnProjectApplication {
@@ -9,5 +15,19 @@ public class LearnProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnProjectApplication.class, args);
 	}
+
+	@Autowired
+	Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("driverClassName"));
+		dataSource.setUrl(env.getProperty("url"));
+		dataSource.setUsername(env.getProperty("user"));
+		dataSource.setPassword(env.getProperty("password"));
+		return dataSource;
+	}
+
 
 }
